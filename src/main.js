21591e85d2a270cc5a9b2606ea42bb2bd04213c8
pranobkale <pranob.kale@decimalpointanalytics.com>
2024-05-20 20:16:53 +0530

    header of portfolio

--- src/main.js
+++ src/main.js
@@ -1,2 +1,6 @@
 import '../styles/modern-normalise.css'
 import '../styles/style.css';
+import "../styles/components/header.css"
+import "../styles/components/hero.css"
+import "../styles/util.css"
+
