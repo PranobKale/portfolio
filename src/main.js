1b819df17817690998b0c52d667a4421de9e9176
pranobkale <pranob.kale@decimalpointanalytics.com>
2024-05-21 19:54:15 +0530

    adding about info

--- src/main.js
+++ src/main.js
@@ -2,5 +2,6 @@ import '../styles/modern-normalise.css'
 import '../styles/style.css';
 import "../styles/components/header.css"
 import "../styles/components/hero.css"
+import "../styles/components/about.css"
 import "../styles/util.css"
 
