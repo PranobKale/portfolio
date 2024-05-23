e47a8dd0e0705ff6bb18ea6d7e402b6fb65cf3a4
pranobkale <pranob.kale@decimalpointanalytics.com>
2024-05-23 16:38:03 +0530

    adding feature and product section

--- src/main.js
+++ src/main.js
@@ -3,5 +3,7 @@ import '../styles/style.css';
 import "../styles/components/header.css"
 import "../styles/components/hero.css"
 import "../styles/components/about.css"
+import "../styles/components/feature.css"
+import "../styles/components/work.css"
 import "../styles/util.css"
 
