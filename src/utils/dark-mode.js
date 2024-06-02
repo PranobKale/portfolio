8b7169600709121a37d969fb8e873ce2fc4f82e9
D8_62964_PranobKale <pranob.kale@decimalpointanalytics.com>
2024-06-02 18:34:47 +0530

    Lazy Loading of images

--- src/utils/dark-mode.js
+++ src/utils/dark-mode.js
@@ -2,11 +2,28 @@ const darkMode = () => {
 
     const themeToggleBtns = document.querySelectorAll('#theme-toggle')
     
-    themeToggleBtns.forEach(btn => {
-        btn.addEventListener('click', () => {
+    // state
+    const theme = localStorage.getItem('theme')
+    
+    // on mount
+    theme && document.body.classList.add(theme)
+    
+    // handler
+    const handleThemeToggle = () => {
             document.body.classList.toggle('light-mode')
-        });
-    });
+            if (document.body.classList.contains('light-mode')){
+                localStorage.setItem('theme','light-mode')
+            }
+            else{
+                localStorage.removeItem('theme')
+                document.body.removeAttribute('class')
+            }
+    };
+    
+    // Event
+    themeToggleBtns.forEach(btn => 
+        btn.addEventListener('click',handleThemeToggle)
+    );
 };
 
 
